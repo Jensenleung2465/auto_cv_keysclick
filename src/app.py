6784a07dfa097400bb3542a6a6ef283058a23430
Jensenleung2465 <121221992+Jensenleung2465@users.noreply.github.com>
2026-04-25 14:31:42 +0800

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,12 @@
+"""
+copyright jensenleung2465 2026.0425 14:30
+"""
+
+import pyautogui
+import time
+
+while True:
+    pyautogui.hotkey('c')
+    time.sleep(500)
+    pyautogui.hotkey('v')
+    time.sleep(100)
